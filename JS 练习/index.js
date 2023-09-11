b9881bd794b9646ee061db3d74f33da8fe4ec315
Flywheel <qq844723532@gmail.com>
2023-09-11 16:27:00 +0800

    CommonJS 和 ES modules 的基本使用

---
+++ JS 练习/index.js
@@ -0,0 +1,11 @@
+const math = require('./math');
+
+
+console.log(math.add(2,5));
+console.log(math.subtract(5,1));
+
+
+const {add} = require('./math')
+
+console.log(add(2,99));
+
---
+++ JS 练习/index.mjs
@@ -0,0 +1,5 @@
+import {add, subtract} from "./math.mjs"
+
+console.log(add(2,5));
+console.log(subtract(5,1));
+
